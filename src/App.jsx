e827efc2b18e9d0705472a9855169895928b94da
Prashant Dasari <dasariprashant0@gmail.com>
2023-09-15 00:15:22 +0530

    Created component files and added them to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import styles from "./style";
+import {
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Navbar,
+  Stats,
+  Testimonials,
+  Hero,
+} from "./components";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="bg-primary w-full text-white">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
 
-export default App
+      <div className={`${styles.flexStart} bg-primary`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
