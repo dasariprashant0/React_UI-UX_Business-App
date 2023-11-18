b0096965c09c1cdaf2376ced9012aa4d1016e8fe
Prashant Dasari <dasariprashant0@gmail.com>
2023-11-18 22:54:41 +0530

    Completed GetStarted component in Hero Section But I Couldn't add a Hover Effect for some reason will have to see later.

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { arrowUp } from '../assets'
+import styles from '../style'
 
 const GetStarted = () => {
   return (
-    <div>GetStarted</div>
-  )
+    <div
+      className={`bg-blue-gradient rounded-full w-[140px] h-[140px] ${styles.flexCenter} p-[2px] cursor-pointer`}
+    >
+      <div
+        className={`bg-primary rounded-full w-[100%] h-[100%] ${styles.flexCenter} flex-col`}
+      >
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
+            <span className="text-gradient">Get</span>
+          </p>
+          <img
+            src={arrowUp}
+            alt="arrow-up"
+            className="w-[23px] h-[23px] object-contain"
+          />
+        </div>
+
+        <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
+          <span className="text-gradient">Started</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default GetStarted
